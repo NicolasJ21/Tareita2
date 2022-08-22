b2b50ea6e0ddefb740215bf058aad656a0769d2d
NicolasJ21 <ni.jelvez@duocuc.cl>
2022-08-22 18:44:02 -0400

    Cosillas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tarea2';
+
+  public Bienvenido(): void{
+    alert('Bienvenido a la pagina')
+  }
+
+  public Emoji(): void{
+    alert('🥵')
+  }
+
+  public ImagenEpica(): void{
+    alert('Hola profe, se como poner imagen epico uwu')
+  }
+
+  public multiplicar(n1:number, n2: number): number{
+    return n1 * n2;
+  }
+
+
+
+
 }
